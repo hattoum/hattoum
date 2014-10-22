d0bbfc2f2f8733f3682fbbc78fe6d0d1c53f440e
hattoum <hattoum96@gmail.com>
2014-10-22 19:49:59 +0200

    //

--- rng.rs
+++ rng.rs
@@ -2,5 +2,5 @@ use std::rand;
 
 fn main() {
 	let a = [(rand::random::<int>() % 100i) + 1i, (rand::random::<int>() % 100i) + 1i, (rand::random::<int>() % 100i) + 1i, (rand::random::<int>() % 100i) + 1i ] ;
-	println!("{}", a[(rand::random::<uint>() % 3u) + 1u]);
+	//println!("{}", a[(rand::random::<uint>() % 3u) + 1u]);
 	}
