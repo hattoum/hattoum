2d9a29640bd7bb00f9097eb9c35e86bb337cbab1
hattoum <hattoum96@gmail.com>
2014-10-22 19:32:34 +0200

    RNG

---
+++ rng.rs
@@ -0,0 +1,6 @@
+use std::rand;
+
+fn main() {
+	let a = [(rand::random::<uint>() % 100u) + 1u, (rand::random::<uint>() % 100u) + 1u, (rand::random::<uint>() % 100u) + 1u, (rand::random::<uint>() % 100u) + 1u ] ;
+	//println!("{}", a[(rand::random::<uint>() % 3u) + 1u]);
+	}
