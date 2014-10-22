6c61e77d7dfdffe9009c71971063eadde4d3cff6
hattoum <hattoum96@gmail.com>
2014-10-22 19:44:03 +0200

    ez katka
    value types changed to int instead of uint

--- rng.rs
+++ rng.rs
@@ -1,6 +1,6 @@
 use std::rand;
 
 fn main() {
-	let a = [(rand::random::<uint>() % 100u) + 1u, (rand::random::<uint>() % 100u) + 1u, (rand::random::<uint>() % 100u) + 1u, (rand::random::<uint>() % 100u) + 1u ] ;
-	//println!("{}", a[(rand::random::<uint>() % 3u) + 1u]);
+	let a = [(rand::random::<int>() % 100i) + 1i, (rand::random::<int>() % 100i) + 1i, (rand::random::<int>() % 100i) + 1i, (rand::random::<int>() % 100i) + 1i ] ;
+	println!("{}", a[(rand::random::<uint>() % 3u) + 1u]);
 	}
